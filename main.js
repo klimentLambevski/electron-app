97e73b4f77d55997535830f3c06bbe70695f76b7
Kliment-Lambevski <kliment.lambevski@gmail.com>
2017-06-23 16:14:38 +0200

    init()

---
+++ main.js
@@ -0,0 +1,60 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const fs = require('fs');
+
+function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
+
+ipcMain.on('image', (event, arg) => {
+    // console.log(arg)  // prints "ping"
+    // event.sender.send('asynchronous-reply', 'pong')
+    console.log('image');
+    fs.writeFile('./image.png', decodeBase64Image(arg).data, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+});
+
+ipcMain.on('synchronous-message', (event, arg) => {
+    console.log(arg)  // prints "ping"
+    event.returnValue = 'pong'
+});
+
+
+
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+    console.log('govna')
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
---
+++ renderer.js
@@ -0,0 +1,26 @@
+const {ipcRenderer} = require('electron');
+console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+    console.log(arg) // prints "pong"
+});
+
+ipcRenderer.send('asynchronous-message', 'ping');
+
+let player = document.querySelector('video');
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let handleSuccess = (stream) => {
+    player.srcObject = stream;
+    canvas.height = canvas.width * (player.offsetHeight / player.offsetWidth);
+};
+
+navigator.mediaDevices.getUserMedia({video: true})
+    .then(handleSuccess);
+
+
+setInterval(() => {
+    ctx.drawImage(player, 0, 0, canvas.width, canvas.height);
+    ipcRenderer.send('image', canvas.toDataURL());
+}, 200);
